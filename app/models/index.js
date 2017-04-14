66aea3d6cebf8f95a6be213f8aab11105766e0da
jasonZ <jasonZ@ys-infoware.com>
2017-04-14 16:13:04 +0800

    加了 sequelize 每次執行的raw sql logger 寫入/log/db-query.log 檔案

--- app/models/index.js
+++ app/models/index.js
@@ -2,22 +2,32 @@ var fs = require('fs'),
   path = require('path'),
   Sequelize = require('sequelize'),
   config = require('../../config/config'),
+  moment = require("moment");
   db = {};
 
 // var sequelize = new Sequelize(config.db);
 
 var sequelize = new Sequelize('ChatAPI', 'sa', '`1qa~!QA', {
   host: '192.168.11.24',
+  //// sequelize 的 log 選項 ， 目前是 print console和寫入 根目錄/log/db-query.log 檔案
+  logging: function (msg) { 
+    console.log(msg);
+    fs.appendFile(path.join(global.appRoot,'log','db-query.log'),moment().toString()+" : "+ msg+"\n", function (err) {
+      if (err) throw err;
+      
+    });
+  },
+
   dialectOptions: {
-  instanceName: 'MSSQLSERVER'
+    instanceName: 'MSSQLSERVER'
   },
   dialect: 'mssql',
   pool: {
-  max: 5,
-  min: 0,
-  idle: 10000
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-  });
+});
 
 fs.readdirSync(__dirname).filter(function (file) {
   return (file.indexOf('.') !== 0) && (file !== 'index.js');
