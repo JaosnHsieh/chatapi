e32f986b8ec17f325e3fc43897270537c31ee476
JaosnHsieh <appleoxxo@gmail.com>
2018-02-25 14:57:14 +0800

    comment out view engine

--- config/express.js
+++ config/express.js
@@ -16,16 +16,16 @@ module.exports = function(app, config, io) {
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == "development";
 
-  app.engine(
-    "handlebars",
-    exphbs({
-      layoutsDir: config.root + "/app/views/layouts/",
-      defaultLayout: "main",
-      partialsDir: [config.root + "/app/views/partials/"]
-    })
-  );
-  app.set("views", config.root + "/app/views");
-  app.set("view engine", "handlebars");
+  // app.engine(
+  //   "handlebars",
+  //   exphbs({
+  //     layoutsDir: config.root + "/app/views/layouts/",
+  //     defaultLayout: "main",
+  //     partialsDir: [config.root + "/app/views/partials/"]
+  //   })
+  // );
+  // app.set("views", config.root + "/app/views");
+  // app.set("view engine", "handlebars");
 
   //存取記錄
 
