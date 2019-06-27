7613645dbad054becbc31df078b430f40bd14614
jasonhsieh <jasonhsieh@ghtinc.com>
2019-06-27 16:08:11 +0800

    update for cors issue

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-var path = require("path");
+var path = require('path');
 global.appRoot = path.resolve(__dirname);
 
-var express = require("express"),
-  config = require("./config/config"),
-  db = require("./app/models");
+var express = require('express'),
+  config = require('./config/config'),
+  db = require('./app/models');
 
-require("babel-register");
-require("babel-polyfill");
+require('babel-register');
+require('babel-polyfill');
 
 var app = express();
+var cors = require('cors');
 
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-var configSocketIo = require("./config/socketio.js");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var configSocketIo = require('./config/socketio.js');
 configSocketIo(io, db);
 
-module.exports = require("./config/express")(app, config, io);
+app.use(
+  cors({
+    origin: (origin, cb) => cb(null, origin),
+    credentials: true,
+  }),
+);
+module.exports = require('./config/express')(app, config, io);
 
 db.sequelize
   .sync()
   .then(function() {
     if (!module.parent) {
       http.listen(config.port, function() {
-        console.log("Express server listening on port " + config.port);
+        console.log('Express server listening on port ' + config.port);
       });
     }
   })
--- config/express.js
+++ config/express.js
@@ -1,90 +1,74 @@
-var express = require("express");
-var glob = require("glob");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var cookieSession = require("cookie-session");
+var express = require('express');
+var glob = require('glob');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 // var session = require('express-session');
-var bodyParser = require("body-parser");
-var compress = require("compression");
-var methodOverride = require("method-override");
-var exphbs = require("express-handlebars");
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 
 module.exports = function(app, config, io) {
-  var env = process.env.NODE_ENV || "development";
+  var env = process.env.NODE_ENV || 'development';
   app.locals.ENV = env;
-  app.locals.ENV_DEVELOPMENT = env == "development";
-  app.enable("trust proxy");
+  app.locals.ENV_DEVELOPMENT = env == 'development';
+  app.enable('trust proxy');
   app.engine(
-    "handlebars",
+    'handlebars',
     exphbs({
-      layoutsDir: config.root + "/app/views/layouts/",
-      defaultLayout: "main",
-      partialsDir: [config.root + "/app/views/partials/"]
-    })
+      layoutsDir: config.root + '/app/views/layouts/',
+      defaultLayout: 'main',
+      partialsDir: [config.root + '/app/views/partials/'],
+    }),
   );
-  app.set("views", config.root + "/app/views");
-  app.set("view engine", "handlebars");
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'handlebars');
 
   //存取記錄
 
   // Logging Starts here
-  var FileStreamRotator = require("file-stream-rotator");
-  var fs = require("fs");
-  var logDirectory = path.join(global.appRoot, "log");
+  var FileStreamRotator = require('file-stream-rotator');
+  var fs = require('fs');
+  var logDirectory = path.join(global.appRoot, 'log');
   // 目錄未建立就建立
   fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
   // 檔案名稱、日期格式及記錄週期設定
   var accessLogStream = FileStreamRotator.getStream({
-    date_format: "YYYYMMDD",
-    filename: logDirectory + "/access-%DATE%.log",
-    frequency: "30d" //記錄週期區間30天
+    date_format: 'YYYYMMDD',
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: '30d', //記錄週期區間30天
   });
 
   // 搭配file-stream-rotator寫入文件 combined是比較詳細的request記錄 是morgan的設定
-  app.use(logger("combined", { stream: accessLogStream }));
+  app.use(logger('combined', { stream: accessLogStream }));
   //簡單的request 記錄輸出到console
-  app.use(logger("dev"));
+  app.use(logger('dev'));
 
   //存取紀錄 END
 
   app.use(bodyParser.json());
   app.use(
     bodyParser.urlencoded({
-      extended: true
-    })
+      extended: true,
+    }),
   );
   app.use(cookieParser());
   app.use(compress());
-  app.use(express.static(config.root + "/public"));
+  app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
   var cookieSessionMiddleware = cookieSession({
-    name: "session",
-    keys: ["qeqw456789a"],
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    name: 'session',
+    keys: ['qeqw456789a'],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
   });
   io.use(function(socket, next) {
     cookieSessionMiddleware(socket.request, socket.request.res, next);
   });
   app.use(cookieSessionMiddleware);
-
-  ////CROS header
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET,POST,PUT,HEAD,DELETE,OPTIONS"
-    );
-    res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
-    res.header("Access-Control-Allow-Credentials", true);
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    return next();
-  });
-  ////CROS header end
 
   // app.use(session({
   //   secret: '123456',
@@ -94,11 +78,11 @@ module.exports = function(app, config, io) {
   //   saveUninitialized: true
   // }));
 
-  app.use("*", (req, res, next) => {
+  app.use('*', (req, res, next) => {
     if (
-      req.baseUrl === "/api/login" ||
-      req.baseUrl === "/api/logout" ||
-      req.baseUrl === "/api/user"
+      req.baseUrl === '/api/login' ||
+      req.baseUrl === '/api/logout' ||
+      req.baseUrl === '/api/user'
     ) {
       return next();
     }
@@ -106,40 +90,40 @@ module.exports = function(app, config, io) {
       next();
     } else {
       console.error(
-        `NOT ALLOWED REQUEST ${req.baseUrl} FROM ${req.ip} DUE TO NOT LOGINED!!`
+        `NOT ALLOWED REQUEST ${req.baseUrl} FROM ${req.ip} DUE TO NOT LOGINED!!`,
       );
       return res.sendStatus(500);
     }
   });
 
-  var controllers = glob.sync(config.root + "/app/controllers/*.js");
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function(controller) {
     require(controller)(app);
   });
 
   app.use(function(req, res, next) {
-    var err = new Error("Not Found");
+    var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
-  if (app.get("env") === "development") {
+  if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
       res.status(err.status || 500);
-      res.render("error", {
+      res.render('error', {
         message: err.message,
         error: err,
-        title: "error"
+        title: 'error',
       });
     });
   }
 
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render("error", {
+    res.render('error', {
       message: err.message,
       error: {},
-      title: "error"
+      title: 'error',
     });
   });
 
